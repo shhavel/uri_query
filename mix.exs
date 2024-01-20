490245c20b09633d3c0915d99be39184554bd0f8
Oleksandr Avoiants <shhavel@gmail.com>
2024-01-20 00:01:47 +0000

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,9 +4,9 @@ defmodule UriQuery.Mixfile do
   def project do
     [app: :uri_query,
      version: "0.2.0",
-     elixir: "~> 1.9",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
+     elixir: "~> 1.16",
+     build_embedded: Mix.env() == :prod,
+     start_permanent: Mix.env() == :prod,
      deps: deps(),
      description: description(),
      package: package()]
@@ -29,7 +29,7 @@ defmodule UriQuery.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:ex_doc, "~> 0.19", only: :dev}]
+    [{:ex_doc, "~> 0.19", only: :dev, runtime: false}]
   end
 
   defp description do
@@ -40,7 +40,7 @@ defmodule UriQuery.Mixfile do
 
   defp package do
     [maintainers: ["Oleksandr Avoiants"],
-     licenses: ["Apache 2.0"],
+     licenses: ["MIT"],
      links: %{"GitHub" => "https://github.com/shhavel/uri_query"},
      files: ~w(mix.exs README.md lib)]
   end
